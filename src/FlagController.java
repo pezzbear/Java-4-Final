247de7529e732e1bb9dbf4b8cd99f55435b16189
mitchell <mitchell@MSI.lan>
2023-01-25 21:18:50 -0600

    Merge branch 'master' of https://github.com/pezzbear/Java-4-Final

--- src/FlagController.java
+++ src/FlagController.java
@@ -1,53 +1,34 @@
-import java.util.Dictionary;
-import java.util.Enumeration;
-import java.util.Hashtable;
+import java.util.*;
 
 public class FlagController {
-	static Dictionary items = new Hashtable();
+	HashMap<String, Boolean> items = new HashMap<String, Boolean>();
 	
-	public static void makedictionary() {
-		items.put("Sword", "0");
-		items.put("Torch", "1");
+	public void makedictionary() {
+		items.put("Sword", false);
+		items.put("Torch", true);
 	}
-	public static void changeboolpos(String item)
-	{
-		String change = (String) items.get(item);
-		int value = Integer.parseInt(change) + 1;
-		items.put(item, Integer.toString(value));
-	}
-	public static void changeboolneg(String item) {
-		String change = (String) items.get(item);
-		int value = Integer.parseInt(change) - 1;
-		items.put(item, Integer.toString(value));
-	}
-	public static boolean getbool(String item) {
-		String thing = (String) items.get(item);
-		boolean bool;
-		if(thing.equals("1"))
-		{
-			bool = true;
-		}
-		else
-		{
-			bool = false;
-		}
-		return bool;
-	}
-	public static void listitems() {
+	
+	public void listitems() {
 		System.out.println("The Items you currently have are:");
-		Enumeration enu = items.keys();
-		while(enu.hasMoreElements())
-		{
-			String placeholder = (String) enu.nextElement();
-			if((String) items.get(placeholder) == "1") {
-				System.out.println(placeholder);
+		for(String name : items.keySet()) {
+			if(items.get(name))
+			{
+				System.out.println(name);
 			}
 		}
 	}
-
-
-	public boolean GetItemFlag() {
-		// TODO Auto-generated method stub
-		return false;
+	
+	public boolean getitem(String item) {
+		boolean itemstatus = items.get(item);
+		return itemstatus;
+	}
+	
+	public void setitem(String item) {
+		if(items.get(item) == false)
+		{
+			items.put(item, true);
+		} else {
+			items.put(item, false);
+		}
 	}
 }
