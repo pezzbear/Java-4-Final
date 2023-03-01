d505be3bab132fe6d92e132da6e52cfe736ca86a
richl <richl@PoundButtons.lan>
2023-03-01 10:22:13 -0600

    Created a EmbeddedDataSource class to resolve SetupDerby class errors

---
+++ src/org/apache/derby/jdbc/EmbeddedDataSource.java
@@ -0,0 +1,22 @@
+package org.apache.derby.jdbc;
+
+import java.sql.Connection;
+
+public class EmbeddedDataSource {
+
+	public void setDatabaseName(String string) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void setCreateDatabase(String string) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public Connection getConnection() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
